71088496bd257aa5a229b98feb349fe8e6ac9765
Shehab H <shehab.web1@gmail.com>
2023-09-12 00:39:41 +0600

    Milestone-7
    Milestone-7

---
+++ Milestone-7/Module 37 - JavaScript You need to know for React/script.js
@@ -0,0 +1,29 @@
+/* Most important 6 concept in JavaScript
+ *
+ * Variables (let, const)
+ * Condition (<, >, <=, >=, ===, !==, &&, ||)
+ * Loop (for loop, while loop)
+ * Array
+ * Function
+ * Object
+ * 
+*/
+
+/* Most important concept in ES6
+ *
+ * Template String
+ * Arrow Function
+ * Spread Operator 
+ * 
+*/
+
+/* Array Method
+ *
+ * map()
+ * forEach()
+ * filter()
+ * find()
+ * 
+*/
+
+/* Array and object Destructuring */
